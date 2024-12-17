4fbb1566a5082967018c12958f1a5c6f04f0a4a4
RealGameXs <rwj.brouwers@gmail.com>
2024-12-18 00:14:40 +0100

    Bug fix

--- Assets/Scripts/Minigame_Advoshooto.cs
+++ Assets/Scripts/Minigame_Advoshooto.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Minigame-Advoshooto : MonoBehaviour
+public class Minigame_Advoshooto : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
