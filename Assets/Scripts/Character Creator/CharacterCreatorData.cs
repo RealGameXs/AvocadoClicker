ee79fcbb0b780916379abd6d41f46d9df3d16c13
lucas lemmens <140121885+ProfessorNether@users.noreply.github.com>
2025-08-21 17:57:33 +0200

    Start character creation

---
+++ Assets/Scripts/Character Creator/CharacterCreatorData.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterCreatorData : MonoBehaviour
+{
+
+    private void Start()
+    {
+        //EnemyData[] enemies = LoadAllFromResources<EnemyData>("Enemies");
+
+        //foreach (var enemy in enemies)
+        //{
+        //    Debug.Log("Loaded enemy: " + enemy.name);
+        //}
+    }
+
+}
